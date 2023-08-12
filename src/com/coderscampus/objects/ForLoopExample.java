71705ba61d07dc8c507dcff3bf9c4cce1e640f74
PrimusAbOris <guy.ross.rit@gmail.com>
2023-08-12 16:45:53 -0400

    Incrementing examples

--- src/com/coderscampus/objects/ForLoopExample.java
+++ src/com/coderscampus/objects/ForLoopExample.java
@@ -3,11 +3,29 @@ package com.coderscampus.objects;
 public class ForLoopExample {
 	void ForLoopExample() {
 		// start point, keep during, thing done each time
-		for(int i = 1; i <= 100; i = i + 1) {
+		for(int i = 1; i <= 100; i++) { // i++ = "i = i + 1"
 			System.out.print(i + ": "); //not *ln
 			System.out.println(i * i);
 		}
 		
 		System.out.println("Done.");
+	}
+	
+	static void incrementExample1(int j) {
+		System.out.println("Going up!");
+		for(j = 1; j <= 10; j++) {
+			System.out.println(j);
+		}
+		
+		System.out.println("Going down!");
+		for(j = 9; j > 10; j--) {
+			System.out.println(j);
+		}
+		
+		System.out.println("Done.");
+	}
+	
+	static void incrementExample2(int j) {
+		System.out.println("Inside incrementExample 2 j: " + j);
 	}
 }
--- src/com/coderscampus/objects/Loops.java
+++ src/com/coderscampus/objects/Loops.java
@@ -3,6 +3,7 @@ package com.coderscampus.objects;
 public class Loops {
 
 	public static void main(String[] args) {
+		/*
 		ForLoopExample forExample1 = new ForLoopExample();
 		forExample1.ForLoopExample();
 		
@@ -11,5 +12,11 @@ public class Loops {
 		
 		WhileLoopExample whileExample2 = new WhileLoopExample();
 		whileExample2.whileLoopExample2();
+		*/
+		int j = 0;
+		ForLoopExample.incrementExample2(++j); //++j increments j FIRST
+		System.out.println("Outside method j: " + j);
+		ForLoopExample.incrementExample2(j++);
+		System.out.println("Outside method j: " + j);
 	}
 }
